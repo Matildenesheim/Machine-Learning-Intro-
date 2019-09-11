c2dbf7e2f8d22387820d93266a19a8e35618a671
Matilde Nesheim <matildenesheim@Matildes-MacBook-Pro.local>
2019-09-11 13:48:21 +0200

    Neural Network trained in predicting gender

--- Intro_to_MachineLearning.py
+++ Intro_to_MachineLearning.py
@@ -23,4 +23,183 @@ n = Neuron(weights,bias)
 
 x = np.array([2,3]) #x1 =2, x2 = 3 
 print(n.feedforward(x)) #0.999
-¢
+
+## Coding a Neural Network: Feedforward ##
+
+class OurNeuralNetwork:
+    #a neural network with:
+    # 2 inputs, a hidden layer with 2 neurons (h1, h2)
+    # an output layer with 1 neuron (o1)
+    #each neuron has the same weights and bias: w = [0,1] and b = 0
+    def __init__(self):
+        self.weights = 0
+        self.bias = bias
+        self.h1 = Neuron(weights,bias)
+        self.h2 = Neuron(weights,bias)
+        self.o1 = Neuron(weights,bias)
+
+        
+    def feedforward(self,x):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        #the inputs for o1 are the outputs from h1 and h2
+        out_o1 = self.o1.feedforward(np.array([out_h1,out_h2]))
+        return(out_o1)
+
+network = OurNeuralNetwork()
+x = np.array([2,3])
+#set the value for weights and bias'
+weights = np.array([0,1])
+bias = 0
+print(network.feedforward(x)) # 0.721 
+
+## Training a neural network, part 1 ## 
+
+def mse_loss(y_true,y_pred):
+    #y_true and y_pred are numpy arrays of the same length
+    return((y_true-y_pred)**2).mean()
+
+y_true = np.array([1,0,0,1])
+y_pred = np.array([0,0,0,0])
+
+print(mse_loss(y_true,y_pred)) #0.5
+
+## THE Complete Neural Network - JUST RUN FROM HERE ## 
+## A neural network trying to learn to classify gender and predict
+def sigmoid(x):
+    #sigmoid activation function: f(x) = 1/(1+e^(-x))
+    return 1/ (1+np.exp(-x))
+
+def deriv_sigmoid(x):
+    #Derivative of sigmoid: f'(x) = f(x) * (1 -f(x))
+    fx = sigmoid(x)
+    return fx*(1-fx)
+
+def mse_loss(y_true,y_pred):
+    #y_true and y_pred are numpy arrays of the same length
+    return((y_true-y_pred)**2).mean()
+
+
+class OurNeuralNetwork:
+    #a neural network with:
+    # 2 inputs, a hidden layer with 2 neurons (h1, h2)
+    # an output layer with 1 neuron (o1)
+    #each neuron has the same weights and bias: w = [0,1] and b = 0
+    def __init__(self):
+        #Weights
+        self.w1 = np.random.normal()
+        self.w2 = np.random.normal()
+        self.w3 = np.random.normal()
+        self.w4 = np.random.normal()
+        self.w5 = np.random.normal()
+        self.w6 = np.random.normal()
+        #Biases
+        self.b1 = np.random.normal()
+        self.b2 = np.random.normal()
+        self.b3 = np.random.normal()
+
+        
+    def feedforward(self,x):
+        #x is a numpy array with 2 elements
+        h1 = sigmoid(self.w1*x[0]+self.w2*x[1]+self.b1)
+        h2 = sigmoid(self.w3*x[0]+self.w2*x[1]+self.b2)
+        #the inputs for o1 are the outputs from h1 and h2
+        o1 = sigmoid(self.w5*h1+self.w6*h2+self.b3)
+
+        return(o1)
+    
+    def train(self,data,all_y_trues):
+        #data is a (n x 2) numpy array, n = # of samples in the dataset. 
+        #all_y_trues is a numpy array with n elements
+        #elements in all_y_trues correspond to those in data
+        learn_rate=0.1
+        epochs = 1000 #number of times to loop through the entire dataset
+
+        for epoch in range(epochs):
+            for x, y_true in zip(data, all_y_trues):
+                #do a feedforward, need these values later
+                sum_h1 = self.w1*x[0]+self.w2*x[1]+self.b1
+                h1 = sigmoid(sum_h1)
+
+                sum_h2 = self.w3*x[0]+self.w4*x[1]+self.b2
+                h2 = sigmoid(sum_h2)
+
+                sum_o1 = self.w5* h1+self.w6*h2 + self.b3
+                o1 = sigmoid(sum_o1)
+                y_pred = o1
+
+                #Calculate partial derivatives 
+                #Naming: d_L_d_w1 represents "partial L/partial w1"
+                d_L_d_ypred = -2*(y_true - y_pred)
+                
+                #neuron o1
+                d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
+                d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
+                d_ypred_d_b3 = deriv_sigmoid(sum_o1)
+
+                d_ypred_d_h1 = self.w5*deriv_sigmoid(sum_o1)
+                d_ypred_d_h2 = self.w6*deriv_sigmoid(sum_o1)
+
+                #neuron h1
+                d_h1_d_w1 = x[0]*deriv_sigmoid(sum_h1)
+                d_h1_d_w2 = x[1]*deriv_sigmoid(sum_h1)
+                d_h1_d_b1 = deriv_sigmoid(sum_h1)
+
+                #neuron h2
+                d_h2_d_w3 = x[0]*deriv_sigmoid(sum_h2)
+                d_h2_d_w4 = x[1]*deriv_sigmoid(sum_h2)
+                d_h2_d_b2 = deriv_sigmoid(sum_h2)
+
+                # Updates weights and biases 
+                #neuron h1
+                self.w1 -=learn_rate*d_L_d_ypred*d_ypred_d_h1 * d_h1_d_w1
+                self.w2 -=learn_rate*d_L_d_ypred*d_ypred_d_h1 * d_h1_d_w2
+                self.b1 -=learn_rate*d_L_d_ypred*d_ypred_d_h1 * d_h1_d_b1
+
+                #neuron h2
+                self.w3 -=learn_rate*d_L_d_ypred*d_ypred_d_h2 * d_h2_d_w3
+                self.w4 -=learn_rate*d_L_d_ypred*d_ypred_d_h2 * d_h2_d_w4
+                self.b2 -=learn_rate*d_L_d_ypred*d_ypred_d_h2 * d_h2_d_b2
+
+                #neuron o1
+                self.w5 -=learn_rate * d_L_d_ypred * d_ypred_d_w5
+                self.w6 -=learn_rate * d_L_d_ypred * d_ypred_d_w6
+                self.b3 -=learn_rate * d_L_d_ypred * d_ypred_d_b3
+
+                #Calculate total loss at the end of each epoch 
+            if epoch % 10 == 0:
+                y_preds = np.apply_along_axis(self.feedforward,1,data)
+                loss = mse_loss(all_y_trues,y_preds)
+                print("Epoch %d loss: %.3f" % (epoch,loss))
+
+#Define dataset
+data = np.array([
+    [-2,-1],    # Alice
+    [25, 6],     # Bob
+    [17, 4],     # Charlie
+    [-15,-6],    #Diana
+])
+all_y_trues =np.array([
+    1,  #Alice
+    0,  #Bob
+    0,  #Charlie
+    1,  #Diana
+])     
+
+#Train the neural network 
+network = OurNeuralNetwork()
+network.train(data, all_y_trues)
+
+## The loss steadily decreases as the network learns
+
+
+## Using the network to predict genders ##
+
+#Making some predictions
+emily = np.array([-7,-3]) #128 pounds, 63 inches
+frank = np.array([20,2]) #155 pounds, 68 inches
+print("Emily: %.3f" % network.feedforward(emily)) #0.951 - F
+print("Frank: %.3f" % network.feedforward(frank)) # 0.039 - M
+
+# IT WORKS:o)
+                
